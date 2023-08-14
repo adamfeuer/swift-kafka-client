503de45202918feae3859cad6774dfeb1f1a108b
Adam Feuer <adam@adamfeuer.com>
2023-08-14 11:46:58 -0700

    moving OpenSSL dependency to Crdkakfa

--- Package.swift
+++ Package.swift
@@ -60,8 +60,8 @@ let package = Package(
         .target(
             name: "Crdkafka",
             dependencies: [
-                "OpenSSL",
                 .product(name: "libzstd", package: "zstd"),
+                .product(name: "OpenSSL", package: "OpenSSL"),
             ],
             exclude: rdkafkaExclude,
             sources: ["./librdkafka/src/"],
@@ -84,7 +84,6 @@ let package = Package(
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "OpenSSL", package: "OpenSSL"),
             ]
         ),
         .target(
