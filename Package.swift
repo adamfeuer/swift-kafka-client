aed18ea8e1e3f5ac003dbad6b0dea826469e8872
Adam Feuer <adam@adamfeuer.com>
2023-08-15 12:51:33 -0700

    changing OpenSSL product package to openssl

--- Package.swift
+++ Package.swift
@@ -61,7 +61,7 @@ let package = Package(
             name: "Crdkafka",
             dependencies: [
                 .product(name: "libzstd", package: "zstd"),
-                .product(name: "OpenSSL", package: "OpenSSL" ),
+                .product(name: "OpenSSL", package: "ssl" ),
             ],
             exclude: rdkafkaExclude,
             sources: ["./librdkafka/src/"],
